f0de90e90bbf60816250cc1a13f2f80c7466217c
William A. Kennington III <william@wkennington.com>
2014-04-28 17:14:28 -0500

    Initial Commit with sample Release

---
+++ release.nix
@@ -0,0 +1,7 @@
+{ mynixpkgs ? { outPath = ./.; revCount = 1234; shortRev = "abcdef"; }
+, nixpkgs
+, packages
+, supportedSystems ? [ "x86_64-linux" "i686-linux" "x86_64-darwin" ]
+}:
+with import "${nixpkgs}/pkgs/top-level/release-lib.nix" { inherit supportedSystems; };
+mapTestOn (map (flip nameValuePair all) packages);
